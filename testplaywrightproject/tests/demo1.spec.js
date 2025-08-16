c6f2e23f7363da4d64f75f60ace2da7133a178aa
pgudi <prabhakargudi@gmail.com>
2025-08-16 17:58:22 +0530

    Initial Draft

---
+++ testplaywrightproject/tests/demo1.spec.js
@@ -0,0 +1,16 @@
+const {test, expect} = require('@playwright/test')
+
+test('First PLaywright Test', async function display({page}){
+    console.log('Running first Playwright test');
+
+})
+
+test('Second PLaywright Test', async function({}){
+    console.log('Running second Playwright test');
+
+})
+
+test('Third PLaywright Test', async ({})=>{
+    console.log('Running third Playwright test');
+
+})
